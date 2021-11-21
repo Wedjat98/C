63da54787778650cc075b1119506c27779fb7b8a
Administrator <11>
2021-11-21 20:41:14 +0900

    moved

--- UsedCode/tempCodeRunnerFile.c
+++ UsedCode/tempCodeRunnerFile.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
- 
-int main()
-{
-    int i, n, t1 = 0, t2 = 1, nextTerm;
- 
-    printf("何項まで？: ");
-    scanf("%d", &n);
- 
-    printf("フィボナッチ数列: ");
- 
-    for (i = 1; i <= n; ++i)
-    {
-        printf("%d, ", t1);
-        nextTerm = t1 + t2;
-        t1 = t2;
-        t2 = nextTerm;
-    }
-    return 0;
+void bubble_sort(int arr[], int len) {
+        int i, j, temp;
+        for (i = 0; i < len - 1; i++)
+                for (j = 0; j < len - 1 - i; j++)
+                        if (arr[j] > arr[j + 1]) {
+                                temp = arr[j];
+                                arr[j] = arr[j + 1];
+                                arr[j + 1] = temp;
+                        }
+}
+int main() {
+        int arr[] = { 22, 34, 3, 32, 82, 55, 89, 50, 37, 5, 64, 35, 9, 70 };
+        int len = (int) sizeof(arr) / sizeof(*arr);//sizeof関数を使って、arrのポインターと割り算するとarr[] の長さがわかる。
+        bubble_sort(arr, len);
+        for (int i = 0; i < len; i++)
+                printf("%d ", arr[i]);
+        return 0;
 }
