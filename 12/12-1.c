34e89c973014e074dbe36efc08b48bc30b5dea2d
Administrator <11>
2021-12-12 19:07:53 +0900

    changed

--- 12/12-1.c
+++ 12/12-1.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <string.h>
-void pin(Person *ptr)
-{
-    printf("Please input age heigh weight: ");
-    scanf("%d%d%d", &*ptr.age,&*ptr.height,&*ptr.weight);
-}
-void pout(Person p)
-{
-    printf("%d%d%d", &p.age,&p.height,&p.weight);
-}
-struct Person
+typedef struct Person
 {
     int age;
     int height;
     int weight
-};
+} Person;
 
 int main()
 {
-    struct Person person;
+    Person person;
     pin(person);
     pout(person);
+}
+void pin(Person *ptr)
+{
+    printf("Please input age heigh weight: ");
+    scanf("%d%d%d", &(ptr)->age,  &(ptr)->height, &(ptr)->weight);
+}
+void pout(Person p)
+{
+    printf("age heigh weight%d%d%d", &p.age, &p.height, &p.weight);
 }
